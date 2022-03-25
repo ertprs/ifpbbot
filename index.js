95973230c14dc268eb1b08591d4961028e65be4c
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-03-25 09:00:54 -0300

    Adicionado .env.sample

--- index.js
+++ index.js
@@ -5,31 +5,43 @@ const log = require('@helpers/logger')
 const makeBox = require('@helpers/makebox')
 const printLogo = require('@helpers/print-logo')
 let error = false
+const {
+	GCLOUD_CREDENTIALS,
+	PROJECT_ID,
+	DISABLE_WHATSAPP,
+	DISABLE_TELEGRAM
+} = process.env
+
 console.clear()
 console.log(makeBox('           IFPB ChatBot           ', chalk.yellowBright, chalk.yellow))
 printLogo()
 
 // Possíveis erros
 try {
-	JSON.parse(process.env.GCLOUD_CREDENTIALS)
+	JSON.parse(GCLOUD_CREDENTIALS)
 } catch {
 	error = true
 	log('redBright', 'Erro')('Credenciais do Dialogflow faltando')
 	log('magentaBright', 'Erro')('Inclua suas credenciais do Dialogflow na variável de ambiente GCLOUD_CREDENTIALS')
-	
 }
 
-if (!process.env.PROJECT_ID) {
+if (!PROJECT_ID) {
 	error = true
 	log('redBright', 'Erro')('Nome do projeto do Dialogflow faltando')
 	log('magentaBright', 'Erro')('Inclua o nome do projeto do Dialogflow na variável de ambiente PROJECT_ID')
 }
 
-if (process.env.DISABLE_WHATSAPP) log('yellowBright', 'Aviso')('Robô do WhatsApp desativado por variável de ambiente')
-if (process.env.DISABLE_TELEGRAM) log('yellowBright', 'Aviso')('Robô do Telegram desativado por variável de ambiente')
+if (isDisabled(DISABLE_WHATSAPP)) log('yellowBright', 'Aviso')('Robô do WhatsApp desativado por variável de ambiente')
+if (isDisabled(DISABLE_TELEGRAM)) log('yellowBright', 'Aviso')('Robô do Telegram desativado por variável de ambiente')
 
 if (error) process.exit()
 
 // Inicia os robôs
-if (!process.env.DISABLE_WHATSAPP) require('./whatsapp')
-if (!process.env.DISABLE_TELEGRAM) require('./telegram')
+if (!isDisabled(DISABLE_WHATSAPP)) require('./whatsapp')
+if (!isDisabled(DISABLE_TELEGRAM)) require('./telegram')
+
+// Retorna false se a string indica que não é para desabilitar
+function isDisabled(string) {
+	const DISABLED_STRINGS = ['false', 'no', '0', '']
+	return !!(string && !DISABLED_STRINGS.includes(string))
+}
