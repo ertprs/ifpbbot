a5f1e7ac517615a5d615403d1794900fc3fc5a5e
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-04-02 13:48:16 -0300

    Novo modo de salvar dados do WhatsApp Web JS

---
+++ modules/whatsapp/backup-data.js
@@ -0,0 +1,125 @@
+const log = require('@helpers/logger')
+const { google } = require('googleapis')
+const path = require('path')
+const fs = require('fs')
+
+const BACKUP_FILE_NAME = 'wwebjs-auth-backup.zip'
+const DOWNLOAD_FILE_NAME = 'wwebjs-auth-backup-download.zip'
+const BACKUP_FILE_PATH = path.join(__dirname, BACKUP_FILE_NAME)
+const DOWNLOAD_FILE_PATH = path.join(__dirname, DOWNLOAD_FILE_NAME)
+const WWEBJS_FOLDER_PATH = path.join(__dirname, '.wwebjs_auth')
+
+const oauth2Client = new google.auth.OAuth2(
+	process.env.GOOGLE_CLIENT_ID,
+	process.env.GOOGLE_CLIENT_SECRET,
+	process.env.GOOGLE_REDIRECT_URI
+)
+
+oauth2Client.setCredentials({
+	refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+})
+
+const drive = google.drive({
+	version: 'v3',
+	auth: oauth2Client
+})
+
+function zipFolder(folderName, outputZipFile) {
+	return new Promise((resolve, reject) => {
+		const archiver = require('archiver')
+		const output = fs.createWriteStream(outputZipFile)
+		const archive = archiver('zip')
+
+		archive.on('warning', (err) => {
+			if (err.code === 'ENOENT') {
+				log('yellowBright', 'WhatsApp - Aviso', err)
+			} else {
+				reject(err)
+			}
+		})
+		archive.on('error', reject)
+		output.on('close', resolve)
+		output.on('end', resolve)
+		archive.directory(folderName, folderName)
+		archive.pipe(output)
+		archive.finalize()
+	})
+}
+
+function uploadFile(fileName, filePath, parentFolder) {
+	return drive.files.create({
+		requestBody: {
+			name: fileName,
+			mimeType: 'application/zip',
+			parents: [parentFolder]
+		},
+		media: {
+			mimeType: 'application/zip',
+			body: fs.createReadStream(filePath)
+		}
+	})
+}
+
+async function downloadFile(fileName, downloadFilePath, parentFolder) {
+	return new Promise(async (resolve, reject) => {
+		const listFilesResponse = await drive.files.list({
+			q: parentFolder ? `"${parentFolder}" in parents` : undefined,
+		})
+
+		const file = listFilesResponse.data.files.find((file) => file.name === fileName)
+		if (!file) return reject(new Error(`Arquivo ${fileName} não encontrado no Google Drive`))
+
+		const dest = fs.createWriteStream(downloadFilePath)
+		const downloadFileResponse = await drive.files.get({
+			fileId: file.id,
+			alt: 'media',
+		}, {
+			responseType: 'stream'
+		})
+
+		downloadFileResponse.data
+			.on('end', resolve)
+			.on('error', reject)
+			.pipe(dest)
+	})
+}
+
+function unzipFolder(downloadFileName, extractFilePath = __dirname) {
+	const extract = require('extract-zip')
+	return extract(downloadFileName, { dir: extractFilePath })
+}
+
+
+async function load() {
+	try {
+		log('yellowBright', 'WhatsApp - WWebJS')('Fazendo o download do arquivo de backup')
+		await downloadFile(BACKUP_FILE_NAME, DOWNLOAD_FILE_PATH, process.env.GOOGLE_DRIVE_BACKUP_FOLDER)
+		fs.rmSync(WWEBJS_FOLDER_PATH, { recursive: true, force: true })
+		log('yellowBright', 'WhatsApp - WWebJS')('Descompactando o arquivo de backup')
+		await unzipFolder(DOWNLOAD_FILE_PATH)
+		log('greenBright', 'WhatsApp - WWebJS')('Arquivo de backup carregado com sucesso')
+	} catch (err) {
+		log('redBright', 'WhatsApp - WWebJS')('Ocorreu um erro ao carregar o arquivo de backup', err)
+	} finally {
+		if (fs.existsSync(DOWNLOAD_FILE_PATH)) fs.unlinkSync(DOWNLOAD_FILE_PATH)
+	}
+}
+
+async function save(client) {
+	try {
+		log('yellowBright', 'WhatsApp - WWebJS', true)('Fechando servidor para backup')
+		await client.destroy()
+		log('yellowBright', 'WhatsApp - WWebJS', true)('Compactando o arquivo de backup')
+		await zipFolder(WWEBJS_FOLDER_PATH, BACKUP_FILE_PATH)
+		client.initialize()
+		log('yellowBright', 'WhatsApp - WWebJS', true)('Fazendo o upload do arquivo de backup')
+		await uploadFile(BACKUP_FILE_NAME, BACKUP_FILE_PATH, process.env.GOOGLE_DRIVE_BACKUP_FOLDER)
+		log('greenBright', 'WhatsApp - WWebJS', true)('Arquivo de backup enviado com sucesso')
+	} catch (err) {
+		log('redBright', 'WhatsApp - WWebJS')('Ocorreu um erro ao enviar o arquivo de backup', err)
+	} finally {
+		if (fs.existsSync(BACKUP_FILE_PATH)) fs.unlinkSync(BACKUP_FILE_PATH)
+	}
+}
+
+module.exports = { load, save }
--- modules/whatsapp/index.js
+++ modules/whatsapp/index.js
@@ -1,107 +1,113 @@
 require('dotenv/config')
 require('module-alias/register')
-const fs = require('fs')
 const path = require('path')
 const qrcode = require('qrcode-terminal')
-const { Client } = require('whatsapp-web.js')
+const { Client, LocalAuth } = require('whatsapp-web.js')
 const ChromeLauncher = require('chrome-launcher')
 const log = require('@helpers/logger')
-const jsonParse = require('@helpers/json-parse')
 const getDFResponse = require('@dialogflow/get-df-response')
+const { load: loadWWebJSBackup, save: saveWWebJSBackup } = require('./backup-data')
 const parseMessages = require('./parse-messages')
 let chromePath
-try { chromePath = ChromeLauncher.Launcher.getInstallations()[0] } catch {}
+try { chromePath = ChromeLauncher.Launcher.getInstallations()[0] } catch { }
 
-const SESSION_FILE_PATH = './wa-session.json'
-
-const client = new Client({
-	session: fs.existsSync(path.resolve(__dirname, SESSION_FILE_PATH))
-		? jsonParse(fs.readFileSync(path.resolve(__dirname, SESSION_FILE_PATH), { encoding: 'utf-8' }))
-		: jsonParse(process.env.WHATSAPP_TOKEN, null, null, true),
-	puppeteer: { executablePath: chromePath, args: ['--no-sandbox'] }
-})
-
-log('yellowBright', 'WhatsApp')('Conectando, aguarde...')
-
-// Quando conectar, salva a sessão no arquivo
-client.on('authenticated', (session) => {
-	fs.writeFile(path.resolve(__dirname, SESSION_FILE_PATH), JSON.stringify(session), () => { })
-})
-
-// Falha na autenticação
-client.on('auth_failure', () => {
-	log('redBright', 'WhatsApp')('Falha na autenticação')
-})
-
-// Desconectado
-client.on('disconnected', () => {
-	log('redBright', 'WhatsApp')('Desconectado')
-})
-
-// Imprime o QR Code
-client.on('qr', (qr) => {
-	log('cyanBright', 'WhatsApp')('Escaneie o QR Code no seu WhatsApp\n')
-	qrcode.generate(qr, { small: true })
-})
-
-// Conectado
-client.on('ready', () => {
-	log('greenBright', 'WhatsApp')('Servidor aberto')
-})
-
-// Nova mensagem
-client.on('message', async (msg) => {
-	// Permite apenas alguns contatos (para testes)
-	if (process.env.WHATSAPP_ALLOWED_NUMBERS) {
-		if (!process.env.WHATSAPP_ALLOWED_NUMBERS.split(',').includes(msg.from)) return
+async function start() {
+	if (
+		process.env.GOOGLE_CLIENT_ID &&
+		process.env.GOOGLE_CLIENT_SECRET &&
+		process.env.GOOGLE_REDIRECT_URI &&
+		process.env.GOOGLE_REFRESH_TOKEN
+	) {
+		await loadWWebJSBackup()
 	}
 
-	const chat = msg.getChat()
+	const client = new Client({
+		authStrategy: new LocalAuth({
+			dataPath: path.join(__dirname, '.wwebjs_auth')
+		}),
+		puppeteer: { executablePath: chromePath, args: ['--no-sandbox'] }
+	})
 
-	// Ativa o estado "Digitando..."
-	chat.then(c => c.sendStateTyping())
+	setInterval(() => saveWWebJSBackup(client), 120000)
+	log('yellowBright', 'WhatsApp')('Conectando, aguarde...')
 
-	if (msg.hasMedia) {
-		return await client
-			.sendMessage(msg.from, '🤷‍♂️ Desculpe, infelizmente não consigo entender áudios e outros arquivos')
-			.catch(console.error)
+	// Falha na autenticação
+	client.on('auth_failure', () => {
+		log('redBright', 'WhatsApp')('Falha na autenticação')
+	})
+
+	// Desconectado
+	client.on('disconnected', () => {
+		log('redBright', 'WhatsApp')('Desconectado')
+	})
+
+	// Imprime o QR Code
+	client.on('qr', (qr) => {
+		log('cyanBright', 'WhatsApp')('Escaneie o QR Code no seu WhatsApp\n')
+		qrcode.generate(qr, { small: true })
+	})
+
+	// Conectado
+	client.on('ready', () => {
+		log('greenBright', 'WhatsApp')('Servidor aberto')
+	})
+
+	// Nova mensagem
+	client.on('message', async (msg) => {
+		// Permite apenas alguns contatos (para testes)
+		if (process.env.WHATSAPP_ALLOWED_NUMBERS) {
+			if (!process.env.WHATSAPP_ALLOWED_NUMBERS.split(',').includes(msg.from)) return
+		}
+
+		const chat = msg.getChat()
+
+		// Ativa o estado "Digitando..."
+		chat.then(c => c.sendStateTyping())
+
+		if (msg.hasMedia) {
+			return await client
+				.sendMessage(msg.from, '🤷‍♂️ Desculpe, infelizmente não consigo entender áudios e outros arquivos')
+				.catch(console.error)
+		}
+
+		// Retorna a resposta do DialogFlow
+		getDFResponse(msg.body, msg.from, 'whatsapp')
+			.then((r) => parseMessages(r, client))
+			.then((m) => sendMessages(m, client, msg))
+			.catch((e) => error(e, msg))
+			.finally(() => {
+				// Desativa o estado "Digitando..."
+				chat.then(c => c.clearState())
+			})
+	})
+
+	// Envia as mensagens
+	async function sendMessages(msgs, client, msg) {
+		for (let res of msgs) {
+			// Se a mensagem for uma Promise
+			if (res.then) res = await res.catch((err) => {
+				console.error(err)
+				return null
+			})
+
+			// Envia apenas se a mensagem for válida
+			if (res) await client.sendMessage(msg.from, res).catch(console.error)
+		}
 	}
 
-	// Retorna a resposta do DialogFlow
-	getDFResponse(msg.body, msg.from, 'whatsapp')
-		.then((r) => parseMessages(r, client))
-		.then((m) => sendMessages(m, client, msg))
-		.catch((e) => error(e, msg))
-		.finally(() => {
-			// Desativa o estado "Digitando..."
-			chat.then(c => c.clearState())
-		})
-})
-
-// Envia as mensagens
-async function sendMessages(msgs, client, msg) {
-	for (let res of msgs) {
-		// Se a mensagem for uma Promise
-		if (res.then) res = await res.catch((err) => {
-			console.error(err)
-			return null
-		})
-
-		// Envia apenas se a mensagem for válida
-		if (res) await client.sendMessage(msg.from, res).catch(console.error)
+	// Executa caso ocorra algum erro
+	function error(err, msg) {
+		console.error(err)
+		client.sendMessage(msg.from, 'Ops! Ocorreu um problema técnico, peço desculpas').catch(console.error)
 	}
+
+	// Inicia o servidor
+	client.initialize()
+
+	return client
 }
-
-// Executa caso ocorra algum erro
-function error(err, msg) {
-	console.error(err)
-	client.sendMessage(msg.from, 'Ops! Ocorreu um problema técnico, peço desculpas').catch(console.error)
-}
-
-// Inicia o servidor
-client.initialize()
 
 // Evita que o servidor feche quando ocorrer um erro
 process.on('uncaughtException', console.error)
 
-module.exports = client
+module.exports = start()
