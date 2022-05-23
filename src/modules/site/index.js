2e0df79829907daaa9704e70593d6dc93d74fdc1
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-05-23 10:24:31 -0300

    fix(site): ID do agente dinâmico

--- src/modules/site/index.js
+++ src/modules/site/index.js
@@ -5,6 +5,31 @@ const app = require('@helpers/http')
 const express = require('express')
 const router = express.Router()
 router.use(express.static(__dirname + '/public'))
+
+router.get('/', (req, res) => {
+	res.send(`
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>IFPB Picuí ChatBot</title>
+	<link rel="stylesheet" href="index.css">
+	<link rel="shortcut icon" href="favicon.png" type="image/png">
+</head>
+
+<body>
+	<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+	<df-messenger chat-icon="logo.png" chat-title="IFPB Picuí ChatBot" agent-id="${process.env.AGENT_ID}"
+		language-code="pt-br" expand="true" intent="WELCOME"></df-messenger>
+	<script src="index.js"></script>
+</body>
+
+</html>
+	`)
+})
+
 app.use('/', router)
 
 const PORT = process.env.PORT || 443
