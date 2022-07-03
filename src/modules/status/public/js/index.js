ea503fdea94626f40924474a1c131ecb03528e9a
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-07-02 21:25:49 -0300

    fix(status): bug de autenticação

--- src/modules/status/public/js/index.js
+++ src/modules/status/public/js/index.js
@@ -1,17 +1,10 @@
-const user = localStorage.getItem('ifpbbot.status.auth.user')
-const password = localStorage.getItem('ifpbbot.status.auth.password')
-
 angular.module('status', []).controller('statusCtrl', ['$scope', '$http', '$timeout', ($scope, $http, $timeout) => {
 	$scope.modules = null
 	$scope.globalStatus = 'connecting'
 	$scope.serverError = null
 
 	function fetchStatus() {
-		return $http.get('/status/data', {
-			headers: {
-				Authorization: `Basic ${btoa(`${user}:${password}`)}`
-			}
-		}).then((response) => {
+		return $http.get('/status/data').then((response) => {
 			$scope.serverError = null
 			$scope.modules = response.data?.modules
 			parseModules($scope.modules)
@@ -25,7 +18,6 @@ angular.module('status', []).controller('statusCtrl', ['$scope', '$http', '$time
 				$scope.globalStatus = 'allOk'
 			}
 		}).catch((err) => {
-			if (err.status === 401) login()
 			console.error(err)
 			$scope.serverError = err
 		}).finally(() => {
@@ -43,13 +35,4 @@ function parseModules(modules) {
 		else if (mod.disabled) mod.status = 'disabled'
 		else if (!mod.started) mod.status = 'starting'
 	}
-}
-
-function login() {
-	if (!confirm('É necessário se autenticar')) return
-	const newUser = prompt('Usuário')
-	const newPassword = prompt('Senha')
-	localStorage.setItem('ifpbbot.status.auth.user', newUser)
-	localStorage.setItem('ifpbbot.status.auth.password', newPassword)
-	window.location.reload()
 }
